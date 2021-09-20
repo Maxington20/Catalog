a6a315c9a544e686b63a501d5d7f1bc28902da98
Maxington20 <max.herrington@gmail.com>
2021-09-20 14:57:23 -0400

    added health check stuff

--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Catalog.Repositories;
 using Catalog.Settings;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -36,11 +40,13 @@ namespace Catalog
             // as they don't handle them natively
             BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
             BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+            var mongoDbSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
 
             services.AddSingleton<IMongoClient>(ServiceProvider => {
                 
-                var settings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-                return new MongoClient(settings.ConnectionString);
+                // movign the code below to the level above this scope
+                // var mongoDbSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+                return new MongoClient(mongoDbSettings.ConnectionString);
             });
 
             // need to add the services that wil be used
@@ -49,12 +55,21 @@ namespace Catalog
 
             services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
 
-
-            services.AddControllers();
+            // the option thing below is to prevent route tampering by .net to not allow it to remove Async at the end of method names
+            services.AddControllers(options => options.SuppressAsyncSuffixInActionNames = false);
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog", Version = "v1" });
             });
+
+            // added to create health checks
+            services.AddHealthChecks()
+                .AddMongoDb(
+                    mongoDbSettings.ConnectionString, 
+                    name: "mongodb", 
+                    timeout: TimeSpan.FromSeconds(3),
+                    // endpoint for if ready to recieve requests 
+                    tags: new[]{"ready"});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -76,6 +91,36 @@ namespace Catalog
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+
+
+                // route for the health check enabled above
+                // first one is for the db
+                endpoints.MapHealthChecks("/healthcheck/ready", new HealthCheckOptions {
+                    Predicate = (check) => check.Tags.Contains("ready"),
+                    ResponseWriter = async(context, report) => 
+                    {
+                        var result = JsonSerializer.Serialize(
+                            new{
+                                status = report.Status.ToString(),
+                                checks = report.Entries.Select(entry => new {
+                                    name = entry.Key,
+                                    status = entry.Value.Status.ToString(),
+                                    exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                                    duration = entry.Value.Duration.ToString()
+                                })
+                            }
+                        );
+
+                        context.Response.ContentType = MediaTypeNames.Application.Json;
+
+                        await context.Response.WriteAsync(result);
+                    }
+                });
+
+                // wehther service is up and running
+                endpoints.MapHealthChecks("/healthcheck/live", new HealthCheckOptions {
+                    Predicate = (_) => false
+                });
             });
         }
     }
